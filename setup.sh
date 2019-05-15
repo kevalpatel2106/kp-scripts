9505505804e62bf575d9b4fb32cbbf3bf47c23c0
Keval Patel <kevalpatel2106@gmail.com>
2019-05-15 13:08:22 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,19 @@
 # Check for App Store updates from apple
  softwareupdate -i -a
 
+# Set xcode tools
+xcode-select —-install
+
 ## Set homebrew
 echo "Setting up Homebrew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install wget curl git node youtube-dl cocoapods openssl
+
+## Install Anaconda
+echo "Setting up Anaconda..."
+brew install python
+read -n 3 -r -p "Download Anaconda... https://anaconda.com/download/#macos. Type yes once installed: (yes)"
+conda create -n py2 python=2.7 anaconda
 
 ## Setup Go
 echo "Setting Go..."
@@ -19,20 +28,6 @@ brew install gradle kotlin
 conda create -n jdk8 openjdk=8.0.152 anaconda
 read -n 3 -r -p "Download Android Studio... https://developer.android.com/studio. Type yes once installed: (yes)"
 
-## Install Anaconda
-echo "Setting up Anaconda..."
-brew install python
-read -n 3 -r -p "Download Anaconda... https://anaconda.com/download/#macos. Type yes once installed: (yes)"
-conda create -n py2 python=2.7 anaconda
-
-## AOSP
-echo "Setting up AOSP..."
-brew install repo
-mkdir ~/Documents/platform-support && cd ~/Documents/platform-support
-source activate py2 && repo init -u https://android.googlesource.com/platform/manifest -b androidx-master-dev && repo sync -j8 -c && source deactivate
-cd ~/Documents
-git clone https://github.com/aosp-mirror/platform_frameworks_base.git
-
 ## Setup GCP
 echo "Setting up GCP..."
 echo "Set the SDK directory to (~/Documents/Go/)"
@@ -44,7 +39,6 @@ gcloud init && echo "y" | gcloud components install
 read -n 3 -r -p "Download (https://getkap.com). Type yes once installed: (yes)"
 read -n 3 -r -p "Download (https://getpostman.com). Type yes once installed: (yes)"
 read -n 3 -r -p "Download (http://chrome.google.com). Type yes once installed: (yes)"
-read -n 3 -r -p "Download (http://iterm2.com). Type yes once installed: (yes)"
 read -n 3 -r -p "Download (https://code.visualstudio.com/download). Type yes once installed: (yes)"
 read -n 3 -r -p "Download (https://www.docker.com/products/docker-desktop). Type yes once installed: (yes)"
 read -n 3 -r -p "Download (https://www.vysor.io). Type yes once installed: (yes)"
@@ -53,12 +47,8 @@ read -n 3 -r -p "Download (https://www.videolan.org/vlc/download-macosx.en-GB.ht
 read -n 3 -r -p "Download (https://sqlitebrowser.org). Type yes once installed: (yes)"
 read -n 3 -r -p "Download (https://www.mozilla.org/en-GB/firefox/). Type yes once installed: (yes)"
 read -n 3 -r -p "Download (Magnet). Type yes once installed: (yes)"
-read -n 3 -r -p "Download (WhatsApp). Type yes once installed: (yes)"
-read -n 3 -r -p "Download (Telegram). Type yes once installed: (yes)"
 read -n 3 -r -p "Download (Bandwidth+). Type yes once installed: (yes)"
 read -n 3 -r -p "Download (OneNote). Type yes once installed: (yes)"
-read -n 3 -r -p "Download (Xcode). Type yes once installed: (yes)"
-read -n 3 -r -p "Download (Jetbrains IDEs). Type yes once installed: (yes)"
 
 
 ## Clean up
@@ -66,3 +56,11 @@ echo "Cleaning up..."
 brew update && brew upgrade && brew cleanup
 echo "y" | conda update --all && conda clean -y -a
 echo "y" | gcloud components update
+
+
+## Set up terminal
+echo "Setting up iTerm2 + zsh..."
+brew cask install iterm2
+brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "Restart NOW!"
